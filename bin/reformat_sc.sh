262a312c2339687d455c9a30763d9ea10f4a8ad1
SYANiDE <chase.hatch.business@gmail.com>
2018-02-24 00:14:05 -0800

    reformat_sc.sh now takes piped stdin\!

--- bin/reformat_sc.sh
+++ bin/reformat_sc.sh
@@ -8,28 +8,44 @@ temp=''
 declare -a the_array
 bytecount=0
 helptxt="One-liner's multi-line shellcode, painlessly, heredoc-style\n[!] USAGE: $0\n"
-if [[ $# -ge 1 ]]; then echo -e "$helptxt"; exit 1; fi
-
-
-echo -e "[!] Enter quoted, multi-line shellcode (sans variable, '=', etc)."
-echo -e "[!] Heredoc-stye. Stop input with line having only 'EOF', no quotes:"
-while [[ "$temp" != "EOF" ]];
-do
-	read -r temp
-	the_array+=("$temp")
-done
+took_pipe=0
+if [[ -p /dev/stdin ]]; then
+        echo "[!] Receiving pipe."
+        while IFS= read line
+        do
+                if [[ $(echo $line |grep -E "\\x") ]]; then
+                        the_array[j]="$line"
+                        let j=$(( $j + 1 ))
+                fi
+        done
+	let took_pipe=$(( $took_pipe + 1))
+else
+	if [[ $# -ge 1 ]]; then echo -e "$helptxt"; exit 1; fi
+	echo -e "[!] Enter quoted, multi-line shellcode (sans variable, '=', etc)."
+	echo -e "[!] Heredoc-stye. Stop input with line having only 'EOF', no quotes:"
+	while [[ "$temp" != "EOF" ]];
+	do
+		read -r temp
+		the_array+=("$temp")
+	done
+fi
 
 
 shellcode=$(echo "${the_array[@]}" |\
-	tr -d '\n' |\
-	sed -e 's/EOF//g' \
-	-e 's/\bunsigned\b//g' \
-	-e 's/\bchar\b//g' \
-	-e 's/\bbuf\b//g' \
-	-e 's/\[\]//g' \
-	-e 's/["; =*]//g' \
-	-e "s/[']//g" \
-	-e 's/.*/\"&\"/')
+		tr -d '\n' |\
+		sed -e 's/EOF//g' \
+		-e 's/\bunsigned\b//g' \
+		-e 's/\bchar\b//g' \
+		-e 's/\bbuf\b//g' \
+		-e 's/\[\]//g' \
+		-e 's/["; =*]//g' \
+		-e "s/[']//g" \
+		-e 's/.*/\"&\"/')
+
+if [[ $took_pipe -eq 1 ]]; then
+	tmp="$(echo $shellcode |sed 's/x/\\x/g')"
+	shellcode=$tmp
+fi
 
 bytecount=$(perl -e "printf($shellcode);" |wc -c)
 
